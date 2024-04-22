490bdf8c750d38aac7499639061f5b1fb9c90bec
marcusthadeu <marcusthadeu@outlook.com>
2024-04-22 11:12:46 -0300

    :recycle:ref: update files

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,6 +1,5 @@
 async function fetchProfileData() {
-  const url = 'https://raw.githubusercontent.com/marcusthadeu/-DIO-Forma-o-JavaScript-Developer-/main/PORTFOLIO/data/profile.json'
-  const response = await fetch(url)
-  const profileData = await response.json()
-  return profileData
+  const url = 'https://raw.githubusercontent.com/marcusthadeu/Portfolio/main/data/profile.json'
+  const fetching = await fetch(url)
+  return await fetching.json()
 }
